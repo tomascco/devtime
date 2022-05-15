99a60e3c65602b8e164d589c50a59006a3a82545
mtayllan <messiastayllan@gmail.com>
2022-05-15 07:34:18 -0300

    ✨ Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   constraints Rodauth::Rails.authenticated do
+    root 'home#index'
+
+    resources :home, only: :index
     resources :hits, only: :index
   end
 
