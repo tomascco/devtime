bc9d52be6aba8435d2540c0f7b8e9c31d45337aa
mtayllan <messiastayllan@gmail.com>
2022-05-15 12:40:14 -0300

    🔧 Configure mailer with dotenv

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {host: ENV["HOST"]}
+
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SMTP_USER_NAME"],
+    password: ENV["SMTP_PASSWORD"],
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
