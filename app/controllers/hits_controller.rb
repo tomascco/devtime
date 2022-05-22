17f8c9da9affbb93478cce0d76e4881416f567d1
mtayllan <messiastayllan@gmail.com>
2022-05-22 15:41:50 -0300

    Reverse the order of hits listing

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -2,6 +2,6 @@ class HitsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @hits = pagy(current_account.hits)
+    @pagy, @hits = pagy(current_account.hits.order(created_at: :desc))
   end
 end
