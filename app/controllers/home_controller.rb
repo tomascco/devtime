fe262dea895097c5a31c55a88fe420c631e58fe3
mtayllan <messiastayllan@gmail.com>
2022-05-22 21:29:58 -0300

    Add total time today stat to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @today_summary = Summary.find_by(account: current_account, day: Time.zone.today)
+    @yesterday_summary = Summary.find_by(account: current_account, day: Time.zone.yesterday)
   end
 end
