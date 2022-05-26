e1ebff55a2d18c89b3737550ef98ea6570a563d6
mtayllan <messiastayllan@gmail.com>
2022-05-26 20:51:08 -0300

    Move processed hits to raw_hits in summary table Need this to save space in free heroku

--- app/models/summary/build.rb
+++ app/models/summary/build.rb
@@ -3,9 +3,9 @@ class Summary::Build < ApplicationJob
   unique :until_executed, on_conflict: :log
 
   MAX_INTERVAL = 900 # 15 minutes
-  def perform(account:)
-    today_hits = account.hits.where("date(timestamp) = ?", Date.current).order(timestamp: :asc)
-    sum = 0
+  def perform(account:, day: Date.current)
+    today_hits = account.hits.where("date(timestamp) = ?", day).order(timestamp: :asc)
+    sum = 1
     last_hit = nil
     today_hits.find_each.with_index do |hit, index|
       if index == 0
@@ -18,8 +18,19 @@ class Summary::Build < ApplicationJob
       last_hit = hit
     end
 
-    summary = Summary.find_or_initialize_by(account_id: account.id, day: Date.current)
-    summary.total_time = sum
+    summary = Summary.find_or_initialize_by(account_id: account.id, day: day)
+    summary.total_time = (summary.total_time || 0) + sum
+    processed_hits = today_hits.map do |hit|
+      attributes = hit.attributes.except(*%w[created_at updated_at account_id id])
+      attributes[:version] = 1
+      attributes
+    end
+    if summary.raw_hits
+      summary.raw_hits.concat(processed_hits)
+    else
+      summary.raw_hits = processed_hits
+    end
     summary.save
+    today_hits.delete_all
   end
 end
