87248a7a6d32c3b59b1253455640468c93ae4b68
mtayllan <messiastayllan@gmail.com>
2022-05-26 21:08:31 -0300

    Fix total_time calculation

--- app/models/summary/build.rb
+++ app/models/summary/build.rb
@@ -5,20 +5,20 @@ class Summary::Build < ApplicationJob
   MAX_INTERVAL = 900 # 15 minutes
   def perform(account:, day: Date.current)
     today_hits = account.hits.where("date(timestamp) = ?", day).order(timestamp: :asc)
-    sum = 1
-    last_hit = nil
-    today_hits.find_each.with_index do |hit, index|
-      if index == 0
-        last_hit = hit
+    summary = Summary.find_or_initialize_by(account_id: account.id, day: day)
+    sum = 0
+    last_hit_time = summary.raw_hits&.last&.[]("timestamp")&.to_time
+    today_hits.find_each do |hit|
+      if last_hit_time.nil?
+        last_hit_time = hit.timestamp
         next
       end
 
-      interval_between_hits = hit.timestamp - last_hit.timestamp
+      interval_between_hits = hit.timestamp - last_hit_time
       sum += interval_between_hits if interval_between_hits < MAX_INTERVAL
-      last_hit = hit
+      last_hit_time = hit.timestamp
     end
 
-    summary = Summary.find_or_initialize_by(account_id: account.id, day: day)
     summary.total_time = (summary.total_time || 0) + sum
     processed_hits = today_hits.map do |hit|
       attributes = hit.attributes.except(*%w[created_at updated_at account_id id])
