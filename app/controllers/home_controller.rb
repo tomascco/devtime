b4012dcc4c2e28bc6e930081a2337932412e6728
mtayllan <messiastayllan@gmail.com>
2022-06-02 23:21:48 -0300

    Sort summaries by day

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @today_summary = Summary.find_by(account: current_account, day: Time.zone.today)
     @yesterday_summary = Summary.find_by(account: current_account, day: Time.zone.yesterday)
-    @week_summaries = Summary.where(account: current_account, day: 6.days.ago..Time.zone.today).pluck(:day, :total_time)
+    @week_summaries = Summary.where(account: current_account, day: 6.days.ago..Time.zone.today).order(:day).pluck(:day, :total_time)
   end
 end
