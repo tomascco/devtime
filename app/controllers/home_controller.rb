cb6777dbc1c246f9ac12fa7ebcb4cef154badbb8
mtayllan <messiastayllan@gmail.com>
2022-06-11 09:57:21 -0300

    ✨ Add date_range filter to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,16 @@ class HomeController < ApplicationController
   def index
     @today_summary = Summary.find_by(account: current_account, day: Time.zone.today)
     @yesterday_summary = Summary.find_by(account: current_account, day: Time.zone.yesterday)
-    @week_summaries = Summary.where(account: current_account, day: 6.days.ago..Time.zone.today).order(:day).pluck(:day, :total_time)
+    @week_summaries = Summary.where(account: current_account, day: date_range).order(:day).pluck(:day, :total_time)
+  end
+
+  private
+
+  def date_range
+    if params[:range] && ApplicationHelper::TIME_RANGES[params[:range]]
+      session[:default_range] = params[:range]
+    end
+    session[:default_range] ||= :last_seven_days
+    ApplicationHelper::TIME_RANGES[session[:default_range]].call
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  TIME_RANGES = {
+    "last_seven_days" => -> { 6.days.ago..Time.zone.today },
+    "this_week" => -> { Time.zone.today.beginning_of_week..Time.zone.today },
+    "past_week" => -> { Time.zone.today.last_week..Time.zone.today.last_week.end_of_week },
+    "last_two_weeks" => -> { Time.zone.today.last_week..Time.zone.today },
+    "this_month" => -> { Time.zone.today.beginning_of_month..Time.zone.today },
+    "past_month" => -> { Time.zone.today.last_month.beginning_of_month..Time.zone.today.last_month.end_of_month }
+  }
 end
