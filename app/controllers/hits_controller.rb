9551f289d4a4edd0f863dc55546cdca4241c9065
mtayllan <messiastayllan@gmail.com>
2022-06-12 10:10:20 -0300

    Add day filter to hits page

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -1,5 +1,14 @@
 class HitsController < ApplicationController
   def index
-    @hits = Summary.where(account: current_account, day: Time.zone.today).pluck(:raw_hits).first
+    @day = get_day
+    @hits = Summary.where(account: current_account, day: @day)&.pluck(:raw_hits)&.first || []
+  end
+
+  private
+
+  def get_day
+    Date.parse(params[:day])
+  rescue
+    Time.zone.today
   end
 end
