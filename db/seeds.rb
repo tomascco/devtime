7528157e8e1b49849d0145ee608ee54216816ba7
mtayllan <messiastayllan@gmail.com>
2022-09-22 20:04:02 -0300

    🌱 Add languages seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Language.insert_all([
+  {
+    name: "ruby",
+    hex_color: "#701516"
+  },
+  {
+    name: "javascript",
+    hex_color: "#F1E05A"
+  },
+  {
+    name: "typescript",
+    hex_color: "#3178C6"
+  },
+  {
+    name: "html",
+    hex_color: "#E34C26"
+  },
+  {
+    name: "css",
+    hex_color: "#563D7C"
+  },
+  {
+    name: "json",
+    hex_color: "#292929"
+  },
+  {
+    name: "python",
+    hex_color: "#3572A5"
+  },
+  {
+    name: "go",
+    hex_color: "#00ADD8"
+  },
+  {
+    name: "java",
+    hex_color: "#B07219"
+  },
+  {
+    name: "c",
+    hex_color: "#555555"
+  },
+  {
+    name: "c++",
+    hex_color: "#F34B7D"
+  },
+  {
+    name: "c#",
+    hex_color: "#178600"
+  },
+  {
+    name: "graphql",
+    hex_color: "#E10098"
+  }
+])
