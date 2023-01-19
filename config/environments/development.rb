66dbac3e16594b9798210ad2881696eb71152af7
mtayllan <messiastayllan@gmail.com>
2023-01-19 19:41:00 -0300

    🔧 Configure development emails to mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = {host: ENV["HOST"], port: ENV["PORT"]}
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: "127.0.0.1", port: 1025}
+  config.action_mailer.raise_delivery_errors = false
 end
